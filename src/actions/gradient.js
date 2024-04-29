3586771cff08dc2ebdf52b0996efa9078582406e
Thomas-Bezille <bezille.thomasb@gmail.com>
2024-04-29 14:48:03 +0200

    ajout line gradient.js

--- src/actions/gradient.js
+++ src/actions/gradient.js
@@ -2,6 +2,8 @@ export const CHANGE_FIRST_COLOR = 'CHANGE_FIRST_COLOR';
 export const CHANGE_LAST_COLOR = 'CHANGE_LAST_COLOR';
 export const CHANGE_DIRECTION = 'CHANGE_DIRECTION';
 
+// -------------------------------------------------- //
+
 export const changeFirstColor = (newColor) => ({
   type: CHANGE_FIRST_COLOR,
   color: newColor,
